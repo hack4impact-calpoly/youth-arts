46190d0694b98d6c0658a58dd67b87bc73eba9e2
JillianQuinn <jillian@jillianq.com>
2021-01-04 19:19:53 -0800

    inital setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello world!')
+})
+
+app.listen(3001)
