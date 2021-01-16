a0bd25665212ebae1980702dd65a34c42ac36ef6
JillianQuinn <jillian@jillianq.com>
2021-01-16 14:38:25 -0800

    Delete <modelName>.js

--- backend/models/<modelName>.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-const modelNameSchema = new mongoose.Schema({
-}, { collection: 'modelName' } )
-const modelName = mongoose.model('modelName', modelNameSchema)
-module.exports = modelName
