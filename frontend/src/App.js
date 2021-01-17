062435e8a1347f0351368a54db7e544b46dc0c83
Tessa Tapmongkol <ttapmong@calpoly.edu>
2021-01-17 15:31:16 -0800

    #10 Added Submit Button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import NavBar from "./Components/NavBar/NavBar.js"
 import Footer from "./Components/Footer/Footer.js"
+import SubmitButton from "./Components/SubmitButton/SubmitButton"
+import React, {useState} from "react";
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+
+  function onClick() {
+    setHeadingText("Submitted");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>{headingText}</h1>
+        <SubmitButton 
+          onClick={onClick}
+          buttonText="LOG IN"
+        />
       </header>
     </div>
   );
