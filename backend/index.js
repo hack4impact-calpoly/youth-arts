8f46a9337ddf2d77f2389c69562ae2a898890e2b
NestorJMartinez <nmarti62@calpoly.edu>
2021-01-17 18:10:41 -0800

    #20 added connection to userDB and get all opportunities

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
+const Opportunity = require('./models/opportunity.js')
+
+require('dotenv').config()
 
 app.use(bodyParser.json())
+
+mongoose.connect(process.env.userDB_URL, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useFindAndModify: false,
+   useCreateIndex: true
+}).then(() => console.log("Connected to userDB"))
 
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
+
+const getAllOpportunities = async () => {
+   return await Opportunity.find({})
+}
 
 app.listen(3001)
