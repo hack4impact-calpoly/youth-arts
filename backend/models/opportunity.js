0586ea1d63d6cbd7eb778c928192f3098a6c203b
NestorJMartinez <nmarti62@calpoly.edu>
2021-02-10 18:57:45 -0800

    #64 updated date attribute for schemas

--- backend/models/opportunity.js
+++ backend/models/opportunity.js
@@ -5,11 +5,11 @@ const opportunitySchema = new mongoose.Schema({
    title: String,
    description: String,
    pictures: [String],
-   date: Date,
+   date: [Date],
    time: Number, 
    skills: [String],
    wishlist: [String],
-   volunteers: [{type: Map, of: {start: Date, end: Date, tasks: [String], donated: [String]}}]
+   volunteers: [{type: Map, of: {start: [Date], end: [Date], tasks: [String], donated: [String]}}]
 }, { collection: 'opportunityDB' })
 
 const Opportunity = mongoose.model('opportunityDB', opportunitySchema)
