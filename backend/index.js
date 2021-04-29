1aa96f9517428b40b509ff036a0df57a5f704207
ameliabruscia <aibrusci@calpoly.edu>
2021-04-29 14:51:33 -0700

    added backend changes

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const Volunteer = require('./models/volunteer')
 const { replaceOne } = require('./models/volunteer')
 const cookieSession = require('cookie-session');
 
+
 app.use(bodyParser.json())
 app.use(
    cors({
