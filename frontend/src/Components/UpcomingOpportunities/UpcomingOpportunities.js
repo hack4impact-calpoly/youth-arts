7eec4fc185ce001173c6999f4600dcc5b5044635
Tessa Tapmongkol <ttapmong@calpoly.edu>
2021-05-16 21:16:31 -0700

    implemented real data on dashboard need help on cancel button

--- frontend/src/Components/UpcomingOpportunities/UpcomingOpportunities.js
+++ frontend/src/Components/UpcomingOpportunities/UpcomingOpportunities.js
@@ -1,5 +1,6 @@
 import "./UpcomingOpportunities.css";
-import {Button} from "react-bootstrap"
+import { Button, Row, Col } from "react-bootstrap"
+import Moment from "moment";
 
 /* 
     props.opps: array of upcoming opportunities (i.e. ["Upcoming Opp 1", "Upcoming Opp 2", ...])
@@ -7,14 +8,34 @@ import {Button} from "react-bootstrap"
 
 function UpcomingOpportunities(props) {
     const opps = props.opps;
+    var oppsArray = Object.values(opps);
+    var oppsToDisplay = []
+
+    // put opportunities into a 1D array
+    for(var i = 0; i < oppsArray.length; i++) {
+        for(var j = 0; j < oppsArray[i].start.length; j++) {
+            var singleOpp = {
+                task: oppsArray[i].task,
+                start: oppsArray[i].start[j],
+                end: oppsArray[i].end[j]
+            }            
+            if(singleOpp !== undefined) oppsToDisplay.push(singleOpp);
+        }
+    }
+
     return (
         <div id="upOpps">
             <h4 id="upType">UPCOMING OPPORTUNTIES</h4>
-            {opps.map((each, index) => (
-                <div>
-                    <p className="upOpp" key={index}>{each}</p>
-                    <Button onClick={() => props.handleCancel(index)} variant="danger" className="p-1 m-2">Cancel</Button>
-                </div>
+            {oppsToDisplay && oppsToDisplay.map((opp, i) => (
+                <Row key={i} className="upOpp">
+                    <Col md="auto" className="col-6">
+                        <p className="upOppTask">{opp.task}</p>
+                        <p className="upOppDate">{Moment(opp.start).format('MMMM Do YYYY, h:mm a') + " to " + Moment(opp.end).format('MMMM Do YYYY, h:mm a')}</p>
+                    </Col>
+                    <Col className="upOppCancel">
+                        <Button variant="danger" onClick={() => props.handleCancel(opp)}>Cancel</Button>
+                    </Col>
+                </Row>
             ))}
         </div>
     );
--- frontend/src/Pages/AuthenticatedUserDashboard/AuthenticatedUserDashboard.js
+++ frontend/src/Pages/AuthenticatedUserDashboard/AuthenticatedUserDashboard.js
@@ -13,15 +13,73 @@ import { useHistory } from "react-router-dom";
 const AuthenticatedUserDashboard = (props) => {
     //sample opportunities
     const { user } = props;
-    const [opps, setOpps] = useState(["Upcoming Opportunity 1", "Upcoming Opportunity 2", "Upcoming Opportunity 3", "Upcoming Opportunity 4", "Upcoming Opportunity 5"]);
+
+    var key, opportunities;
+
+    if(user && (user.opportunities !== null || user.opportunities !== undefined)) {
+        Object.keys(user.opportunities).map((item) => {
+            key = item;
+            return null;
+        })
+        if(key !== null || key !== undefined) opportunities = user.opportunities[key];
+        else opportunities = [];
+    }
+    else opportunities = []
+
+    const [opps, setOpps] = useState(opportunities);
     const history = useHistory();
 
-    function handleCancel(index) {
-        const updated = opps.filter((opp, i) => {
-            return i !== index
-        });
+    function handleCancel(cancelOpp) {
+        var updated = opps;
+        for(var i = 0; i < opps.length; i++) {
+            if(opps[i].task === cancelOpp.task) {
+                for(var j = 0; j < opps[i].start.length; j++) {
+                    if(cancelOpp.start === opps[i].start[j] && cancelOpp.end === opps[i].end[j]) {
+                        opps[i].start.splice(j, 1);
+                        opps[i].end.splice(j, 1);
+                    }
+                }
+            }
+        }
         setOpps(updated);
+
+        var newOpportunites = user.opportunities;
+        newOpportunites[key] = updated;
+
+        const updateVolunteer = {
+            _id: props.user._id,
+            AOI: props.user.AOI,
+            address: props.user.address,
+            boardMember: props.user.boardMember,
+            communityRole: props.user.communityRole,
+            email: props.user.email,
+            experience: props.user.experience,
+            firstName: props.user.firstName,
+            googleId: props.user.googleId,
+            lastName: props.user.lastName,
+            outreach: props.user.outreach,
+            phoneNum: props.user.phoneNum,
+            signature: props.user.signature,
+            tasks: props.user.tasks,
+            username: props.user.username,
+            workHistory: props.user.workHistory,
+            _v: props.user._v,
+            opportunities: newOpportunites
+        }
+
+        fetch(`${process.env.REACT_APP_SERVER_URL}/api/updateVolunteer`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updateVolunteer)
+        }).then(response => {
+            response.json().then(data => {
+                console.log("Successful" + data);
+            });
+        });       
     } 
+
     const refreshOnLogin = () => {
         history.push("/");
         setTimeout(() => history.push(history.push("/")), 10);
