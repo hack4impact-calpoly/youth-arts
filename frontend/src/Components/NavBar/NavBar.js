b1f1e2a3352bdb7810548399e151a7eea96c0284
JillianQuinn <jillian@jillianq.com>
2021-05-25 17:19:35 -0700

    reports export

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -25,16 +25,14 @@ const NavBar = (props) => {
               <li><Link to="/">Dashboard</Link></li>
               <li><Link to="/opportunities" >Opportunities</Link></li>
               <li><Link to="/Calendar">Calendar</Link></li>
-              <li><Link to="/Reports">Reports</Link></li>
-              {/* {user && user.admin !== null && user.admin === true ? <li><a href={`${process.env.REACT_APP_CLIENT_URL}/directory`}>Directory</a></li> : null} */}
+              {user && user.admin !== null && user.admin === true ? <li><Link to="/Reports">Reports</Link></li> : null}
               {user && user.admin !== null && user.admin === true ? <li><Link to="/directory">Directory</Link></li> : null}
               <li><Link to="/FAQ">FAQ</Link></li>
-              <li><Link to="/OpportunityCheckout">Cart</Link></li>
               {user && user.boardMember !== null && user.boardMember === true ? <li><a href={`${process.env.REACT_APP_CLIENT_URL}logHours`}>Log Hours</a></li> : null}
               { user ? <li><Link to="/registration">Profile</Link></li> : null }
+              { user ? <li><Link to="/OpportunityCheckout">Cart</Link></li> : null }
               { user ? (
                 <li><a href="/" onClick={logout}>Log Out</a></li>
-                // <li><a href={`${process.env.REACT_APP_SERVER_URL}/auth/logout`}>Log Out</a></li>
               ) : 
                 <li><Link to="/Login">Login</Link></li>
               }
--- frontend/src/Pages/ReportsPage/ReportsPage.js
+++ frontend/src/Pages/ReportsPage/ReportsPage.js
@@ -11,6 +11,11 @@ const ReportsPage = (props) => {
             <div id="repHeader">
                 <h1 className="repTitle">REPORTS</h1>
             </div>
+            <div className="reportsTextWrapper">
+                <h4 className="reportsText">To export all opportunity data, click 'EXPORT' in the top toolbar of the table. </h4>
+                <h4 className="reportsText">To export all the volunteer's information for a particular opportunity, click 'Export Volunteers'.</h4>
+                <h4 className="reportsText">To export just the volunteer's phone numbers, click 'Export Phone Numbers'.</h4>
+            </div>
             <ReportsSearchOpportunities user={user}/>
         </body>
     );
