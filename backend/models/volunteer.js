f87683696119afe29ba7f0b84ba4fc5d3b6fc95a
Lauren Allen <81931380+lallen15@users.noreply.github.com>
2023-05-01 13:49:53 -0700

    "feat: added board member button"

--- backend/models/volunteer.js
+++ backend/models/volunteer.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const passportLocalMongoose = require("passport-local-mongoose");
 const findOrCreate = require("mongoose-findorcreate");
-const { volunteerDB, opportunityDB } = require("../connections");
+const { volunteerDB } = require("../connections");
 
 const volunteerSchema = new mongoose.Schema(
   {
